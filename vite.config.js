ec825b9fbd119bb3944e67356557ec8ca7e463f3
anhminh270622 <20020444@vnu.edu.vn>
2024-04-15 11:09:20 +0700

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'build'
+  },
   plugins: [react(),
   TanStackRouterVite()],
 })
